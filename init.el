4ca7b3292e7692d843e7bf290d5da02d60d822f6
Jake Song <jake@xlgames.com>
2016-04-17 02:24:19 +0900

    - 폰트 리가쳐가 되게 함 - 옴니 서버를 죽일 건지 물어보지 않게 함

--- init.el
+++ init.el
@@ -9,8 +9,9 @@
 (setq make-backup-files nil)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
-(global-linum-mode t)
-(global-hl-line-mode t)
+(global-linum-mode +1)
+(global-hl-line-mode +1)
+(mac-auto-operator-composition-mode +1)
 
 (desktop-save-mode 1)
 (when window-system (set-frame-size (selected-frame) 212 80))
@@ -34,12 +35,12 @@
   (package-install 'alchemist))
 
 (require 'elixir-mode)
-(add-to-list 'elixir-mode-hook
-             (defun auto-activate-ruby-end-mode-for-elixir-mode ()
-               (set (make-variable-buffer-local 'ruby-end-expand-keywords-before-re)
-                    "\\(?:^\\|\\s-+\\)\\(?:do\\)")
-               (set (make-variable-buffer-local 'ruby-end-check-statement-modifiers) nil)
-               (ruby-end-mode +1)))
+(add-hook 'elixir-mode-hook
+		  (lambda ()
+			(set (make-variable-buffer-local 'ruby-end-expand-keywords-before-re)
+				 "\\(?:^\\|\\s-+\\)\\(?:do\\)")
+			(set (make-variable-buffer-local 'ruby-end-check-statement-modifiers) nil)
+			(ruby-end-mode +1)))
 
 										; smartparens 모드
 
@@ -65,29 +66,44 @@
 
 										; company 모드
 
-(add-hook 'after-init-hook 'global-company-mode)
+(add-hook 'after-init-hook #'global-company-mode)
 (eval-after-load 'company
-  '(add-to-list 'company-backends 'company-omnisharp))
+  '(progn
+	 (define-key company-active-map (kbd "<tab>") 'company-select-next)
+	 (define-key company-active-map (kbd "<S-tab>") 'company-select-previous)
+	 (add-to-list 'company-backends 'company-omnisharp)
+	 (company-flx-mode +1)))
 
 										; c# 모드
 
 (add-hook 'csharp-mode-hook 'omnisharp-mode)
 (add-hook 'csharp-mode-hook 'evil-smartparens-mode)
-(add-hook 'find-file-hook
-		  (lambda ()
-			(set-process-query-on-exit-flag (get-process "Omni-Server") nil)
-			(set-process-query-on-exit-flag (get-process "Omni-Server<1>") nil)))
+(add-hook 'csharp-mode-hook 'set-exit-flags t)
+(defun set-exit-flags ()
+  (set-exit-flag "Omni-Server")
+  (set-exit-flag "Omni-Server<1>"))
+(defun set-exit-flag (process)
+  (let ((omni-server (get-process process)))
+	(if omni-server
+		(set-process-query-on-exit-flag omni-server nil)
+	  nil)))
+(add-hook 'omnisharp-mode-hook 'set-exit-flags)
 
 										; 키보드
 
+(global-set-key (kbd "M-x") 'helm-M-x)
 (global-set-key "\M-\\" 'evil-window-vsplit)
+(global-set-key "\M-b" 'helm-buffers-list)
 (global-set-key "\M-p" 'projectile-find-file)
 (global-set-key "\M-s" 'save-buffer)
 (global-set-key "\M-w" 'evil-window-delete)
+(evil-define-key 'normal omnisharp-mode-map (kbd "<f2>") 'omnisharp-rename)
 (evil-define-key 'normal omnisharp-mode-map (kbd "<f12>") 'omnisharp-go-to-definition)
+(evil-define-key 'normal omnisharp-mode-map (kbd "<S-f12>") 'omnisharp-find-usages)
 (evil-leader/set-leader "<SPC>")
 (evil-leader/set-key "b" 'ido-switch-buffer)
-(evil-leader/set-key "g" 'magit-status)
+(evil-leader/set-key "h" 'evil-window-left)
+(evil-leader/set-key "l" 'evil-window-right)
 (evil-leader/set-key "q" 'save-buffers-kill-terminal)
 (evil-leader/set-key "r" 'eval-region)
 
@@ -101,12 +117,23 @@
 
 (require 'fill-column-indicator)
 (setq-default fill-column 100)
-(add-hook 'find-file-hook 'fci-mode)
+(add-hook 'find-file-hook #'fci-mode)
 
 										; projectile
 
 (projectile-global-mode)
 (setq projectile-enable-caching t)
+
+										; helm
+
+(require 'helm-config)
+(helm-mode 1)
+
+										; spaceline
+
+(require 'spaceline-config)
+(spaceline-spacemacs-theme)
+(setq spaceline-highlight-face-func #'spaceline-highlight-face-evil-state)
 
 										; custom 모드
 
@@ -118,21 +145,22 @@
  '(custom-enabled-themes (quote (sanityinc-tomorrow-eighties)))
  '(custom-safe-themes
    (quote
-	("628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" default)))
+	("e87a2bd5abc8448f8676365692e908b709b93f2d3869c42a4371223aab7d9cf8" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" default)))
  '(desktop-restore-eager 3)
  '(linum-format "%4d")
  '(omnisharp-server-executable-path
    "~/Downloads/omnisharp-server/OmniSharp/bin/Debug/OmniSharp.exe")
+ '(powerline-default-separator (quote arrow))
+ '(powerline-height 20)
  '(tab-width 4))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(linum ((t (:background "#2d2d2d" :foreground "#999999" :slant normal :height 0.7)))))
+ '(linum ((t (:background "#2d2d2d" :foreground "#999999" :slant normal :height 0.7))))
+ '(sp-show-pair-match-face ((t (:inherit default :underline (:color "yellow" :style wave))))))
 										; 칼라 테마 (커스텀 보다 밑에 있어야 함)
 
 (require 'color-theme-sanityinc-tomorrow)
 (color-theme-sanityinc-tomorrow-eighties)
-;; (load-theme 'monokai t)
-;(load-theme 'leuven t)
